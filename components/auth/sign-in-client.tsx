93b6360d95309c932a6188d3c1c20f91f569202b
Navin Avadhani <knavinavadhani@gmail.com>
2023-12-10 17:53:25 +0530

    style: Fixed sign in client logo size.

--- components/auth/sign-in-client.tsx
+++ components/auth/sign-in-client.tsx
@@ -5,7 +5,7 @@ import { ThemeToggleButton } from "@/components/theme-toggle-button";
 export default function SignInClient() {
   return (
     <div className="flex h-full -translate-y-3 flex-col items-center justify-center gap-2">
-      <Logo />
+      <Logo width={70} height={70} />
       <h1 className="mt-4 text-3xl font-bold">Welcome to Helpdesk</h1>
       <SignInScreen />
     </div>
